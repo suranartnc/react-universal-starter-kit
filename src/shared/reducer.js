829fbb02a320cdd1435b72676105fccec910b8ca
SuranartN <suranartnc@gmail.com>
2016-09-02 22:02:19 +0700

    add entities reducer

--- src/shared/reducer.js
+++ src/shared/reducer.js
@@ -1,11 +1,18 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
+import merge from 'lodash/merge'
 
-import postReducer from 'shared/modules/post/postReducer'
+function entities(state = { posts: {} }, action) {
+  if (action.response && action.response.entities) {
+    return merge({}, state, action.response.entities)
+  }
+
+  return state
+}
 
 const rootReducer = combineReducers({
   routing: routerReducer,
-  post: postReducer
+  entities: entities
 })
 
 export default rootReducer
