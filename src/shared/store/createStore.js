370f5878f3810c85e826060e024d38b3923fb150
SuranartN <suranartnc@gmail.com>
2016-09-03 23:38:55 +0700

    refactor reducers

--- src/shared/store/createStore.js
+++ src/shared/store/createStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose  } from 'redux';
 
 import apiMiddleware from 'shared/middlewares/apiMiddleware';
 import { routerMiddleware } from 'react-router-redux';
-import rootReducer from 'shared/reducer';
+import rootReducer from 'shared/reducers/rootReducer';
 
 export default (history, initialState) => {
   
@@ -24,8 +24,8 @@ export default (history, initialState) => {
   )
 
   if (module.hot) {
-    module.hot.accept('shared/reducer', () => {
-      const nextReducer = require('shared/reducer').default;
+    module.hot.accept('shared/reducers/rootReducer', () => {
+      const nextReducer = require('shared/reducers/rootReducer').default;
       store.replaceReducer(nextReducer);
     });
   }
