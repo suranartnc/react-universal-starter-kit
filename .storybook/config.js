4dc9ee52cafbd0dd71ff2410a231aef5e27f0058
TaToonz <peerapath.t@gmail.com>
2016-09-07 08:08:23 +0700

    added CenterDecorator for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,12 @@
-import { configure } from '@kadira/storybook'
+import { configure, addDecorator } from '@kadira/storybook'
+import CenterDecorator from './decorators/CenterDecorator'
+
+const req = require.context('../stories', true, /.js$/)
+
+addDecorator(CenterDecorator)
 
 function loadStories() {
-	require('../stories/demo')
+  req.keys().forEach(filename => req(filename))
 }
 
 configure(loadStories, module)
