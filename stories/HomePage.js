389614e2a8ec93e47c379411eb958b9a9fa44ccf
TaToonz <peerapath.t@gmail.com>
2016-09-07 09:43:48 +0700

    added HomePage stories

---
+++ stories/HomePage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import faker from 'faker'
+import HomePage from 'shared/pages/HomePage/HomePage'
+
+const posts = [];
+for (let i = 0; i < 5; ++i) {
+  posts.push({
+    id: i + 1,
+    title: faker.lorem.sentence(),
+    excerpt: faker.lorem.paragraphs(2),
+    body: faker.lorem.paragraphs(10),
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    avatar: faker.image.avatar(),
+    tags: faker.lorem.sentence().replace('.', '').split(''),
+    pubDate: '2 hours ago'
+  })
+}
+
+storiesOf('Home Page', module)
+  .add('default', () => (
+    <HomePage posts={posts} />
+  ))
