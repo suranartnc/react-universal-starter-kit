495947351518d24e22d7ca8cf3057122e65e42f2
TaToonz <peerapath.t@gmail.com>
2016-09-10 12:55:44 +0700

    implements handleKeyCommand

--- src/shared/pages/WritePage/WriteEditor.js
+++ src/shared/pages/WritePage/WriteEditor.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import { Editor, EditorState } from 'draft-js'
+import { Editor, EditorState, RichUtils } from 'draft-js'
 import { stateToHTML } from 'draft-js-export-html'
 
 class WriteEditor extends Component {
@@ -16,9 +16,22 @@ class WriteEditor extends Component {
     this.props.onChange(html)
   }
 
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
+
+    if (newState) {
+      this.onChange(newState)
+      return 'handled'
+    }
+
+    return 'not-handled'
+  }
+
   render() {
     return (
-      <Editor editorState={this.state.editorState} onChange={this.onChange} />
+      <Editor editorState={this.state.editorState}
+        onChange={this.onChange}
+        handleKeyCommand={this.handleKeyCommand} />
     )
   }
 }
