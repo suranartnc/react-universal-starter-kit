292f7dfadbcb50633aad7aff27eac95021ebcfdf
TaToonz <peerapath.t@gmail.com>
2016-09-13 23:08:22 +0700

    created link button and link input

--- src/shared/components/TextEditor/Toolbar/Toolbar.js
+++ src/shared/components/TextEditor/Toolbar/Toolbar.js
@@ -14,6 +14,11 @@ const BLOCK_TYPE_BUTTONS = [
 ]
 
 class Toolbar extends Component {
+  state = {
+    showLinkInput: false,
+    linkValue: '',
+  }
+
   renderInlineStyleButtons() {
     const currentStyle = this.props.editorState.getCurrentInlineStyle()
 
@@ -27,11 +32,7 @@ class Toolbar extends Component {
         />
     ))
 
-    return (
-      <div className="btn-group" role="group">
-        {buttons}
-      </div>
-    )
+    return buttons
   }
 
   toggleInlineStyle = (inlineStyle) => {
@@ -56,11 +57,20 @@ class Toolbar extends Component {
       />
     ))
 
-    return (
-      <div className="btn-group" role="group">
-        {buttons}
-      </div>
-    )
+    return buttons
+  }
+
+  getCurrentBlockType() {
+    const selection = this.getCurrentSelection()
+    return this.props.editorState
+      .getCurrentContent()
+      .getBlockForKey(selection.getStartKey())
+      .getType()
+  }
+
+  getCurrentSelection() {
+    const { editorState } = this.props
+    return editorState.getSelection()
   }
 
   toggleBlockType = (blockType) => {
@@ -72,20 +82,85 @@ class Toolbar extends Component {
     )
   }
 
-  getCurrentBlockType() {
-    const { editorState } = this.props
-    const selection = editorState.getSelection()
-    return editorState
-      .getCurrentContent()
-      .getBlockForKey(selection.getStartKey())
-      .getType()
+  renderLinkButton() {
+    return (
+      <div>
+        <button className="btn btn-default"
+          disabled={! this.hasSelection() && 'disabled'}
+          onMouseDown={this.toggleLink}
+        >
+          Link
+        </button>
+        {this.state.showLinkInput && this.renderLinkInput()}
+      </div>
+    )
+  }
+
+  hasSelection() {
+    const selection = this.getCurrentSelection()
+    return ! selection.isCollapsed()
+  }
+
+  toggleLink = () => {
+    // TODO:
+    // - check current block already has a link or not for toggle url input or unlink
+    // - close link input
+    this.promptForLink()
+  }
+
+  promptForLink = () => {
+    this.setState({
+      showLinkInput: true
+    }, () => {
+      // this trick from draft-js link example
+      setTimeout(() => this.refs.url.focus(), 0)
+    })
+  }
+
+  renderLinkInput() {
+    return (
+      <input ref="url"
+        type="url"
+        className="form-control"
+        placeholder="Paste or type a link"
+        onChange={this.onLinkChange}
+        onKeyDown={this.onLinkInputKeyDown} />
+    )
+  }
+
+  onLinkChange = (e) => {
+    this.setState({
+      linkValue: e.target.value
+    })
+  }
+
+  onLinkInputKeyDown = (e) => {
+    if (e.which === 13) {
+      this.confirmLink(e)
+    }
+  }
+
+  confirmLink = (e) => {
+    e.preventDefault()
+    console.log(this.state.linkValue)
+
+    // TODO:
+    // - validate link
+    // - create entity type link with draft-js decorator
   }
 
   render() {
+    // TODO: find a way to render link button along with link input
+
     return (
       <div className="btn-toolbar" role="toolbar">
-        {this.renderInlineStyleButtons()}
-        {this.renderBlockTypeButtons()}
+        <div className="btn-group" role="group">
+          {this.renderInlineStyleButtons()}
+          {this.renderLinkButton()}
+        </div>
+        <div className="btn-group" role="group">
+          {this.renderBlockTypeButtons()}
+        </div>
       </div>
     )
   }
