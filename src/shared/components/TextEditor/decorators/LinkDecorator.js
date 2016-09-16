5574d98dac5f47b8854a445d7797b9df210f4bde
TaToonz <peerapath.t@gmail.com>
2016-09-16 17:35:58 +0700

    editor can convert plaintext url to link component

--- src/shared/components/TextEditor/decorators/LinkDecorator.js
+++ src/shared/components/TextEditor/decorators/LinkDecorator.js
@@ -1,25 +1,48 @@
 import React from 'react'
 import { Entity } from 'draft-js'
+import linkifyIt from 'linkify-it'
+import tlds from 'tlds'
+
+const linkify = linkifyIt()
+linkify.tlds(tlds)
 
 export const LinkEntityType = 'LINK'
 
-export const LinkEntity = ({ entityKey, children}) => {
-  const { url } = Entity.get(entityKey).getData()
+export const LinkEntity = (props) => {
+  const { entityKey, decoratedText, children} = props
+
+  let url = decoratedText
+  if (entityKey !== null) {
+    url = Entity.get(entityKey).getData().url
+  }
+  url = url.trim()
 
   return (
-    <a href={url}>
+    <a href={url} target="_blank" rel="nofollow">
       {children}
     </a>
   )
 }
 
 export const LinkStrategy = (contentBlock, callback) => {
-  contentBlock.findEntityRanges(findEntityRanges, callback)
+  filterByEntityType(contentBlock, callback)
+  filterByPlainText(contentBlock, callback)
 }
 
-function findEntityRanges(character) {
-  const entityKey = character.getEntity()
-  return entityKey !== null && Entity.get(entityKey).getType() === LinkEntityType
+function filterByEntityType(contentBlock, callback) {
+  contentBlock.findEntityRanges((character) => {
+    const entityKey = character.getEntity()
+    return entityKey !== null && Entity.get(entityKey).getType() === LinkEntityType
+  }, callback)
+}
+
+function filterByPlainText(contentBlock, callback) {
+  const links = linkify.match(contentBlock.getText())
+  if (links !== null) {
+    links.forEach((link) => {
+      callback(link.index, link.lastIndex)
+    })
+  }
 }
 
 export default {
