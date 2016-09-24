c25c34ba0cc64bb82973436c4be782904bcd28a1
TaToonz <peerapath.t@gmail.com>
2016-09-24 19:48:32 +0700

    text editor changes from draft-js to medium-editor

--- src/shared/components/TextEditor/TextEditor.js
+++ src/shared/components/TextEditor/TextEditor.js
@@ -1,82 +1,44 @@
 import React, { Component, PropTypes } from 'react'
-import { Editor, EditorState, RichUtils } from 'draft-js'
-import { stateToHTML } from 'draft-js-export-html'
-import InlineStyleControls from './InlineStyleControls/InlineStyleControls'
-import BlockStyleControls from './BlockStyleControls/BlockStyleControls'
+import ReactDOM from 'react-dom'
+
+import 'medium-editor/dist/css/medium-editor.css'
+import 'medium-editor/dist/css/themes/default.css'
+
 import styles from './TextEditor.scss'
 
 class TextEditor extends Component {
-  state = {
-    editorState: EditorState.createEmpty()
+  componentDidMount() {
+    if (typeof document !== 'undefined') {
+      this.createMediumEditor()
+    }
   }
 
-  onChange = (editorState) => {
-    this.setState({
-      editorState
+  createMediumEditor() {
+    const MediumEditor = require('medium-editor')
+    const dom = ReactDOM.findDOMNode(this)
+
+    const { label, onChange } = this.props
+
+    this.editor = new MediumEditor(dom, {
+      placeholder: {
+        text: label,
+        hideOnClick: false,
+      },
+      autoLink: true,
     })
 
-    const html = stateToHTML(editorState.getCurrentContent())
-    this.props.onChange(html)
+    this.editor.subscribe('editableInput', (data, editable) => {
+      onChange(dom.innerHTML)
+    })
   }
 
-  onFocus = (e) => {
-    this.refs.editor.focus()
-    this.props.onFocus(e)
-  }
-
-  handleKeyCommand = (command) => {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
-
-    if (newState) {
-      this.onChange(newState)
-      return true
-    }
-
-    return false
-  }
-
-  toggleInlineStyle = (inlineStyle) => {
-    const newState = RichUtils.toggleInlineStyle(this.state.editorState, inlineStyle)
-    this.onChange(newState)
-  }
-
-  toggleBlockType = (blockType) => {
-    const newState = RichUtils.toggleBlockType(this.state.editorState, blockType)
-    this.onChange(newState)
+  componentWillUmmount() {
+    this.editor.destroy()
   }
 
   render() {
-    console.log(styles)
-
-    const { editorState } = this.state
-
-    return (
-      <div>
-        <div className="btn-toolbar" role="toolbar">
-          <InlineStyleControls
-            editorState={editorState}
-            onToggle={this.toggleInlineStyle} />
-
-          <BlockStyleControls
-            editorState={editorState}
-            onToggle={this.toggleBlockType} />
-        </div>
-
-        <div className={styles.editor} onClick={this.onFocus}>
-          <Editor ref="editor"
-            editorState={editorState}
-            onChange={this.onChange}
-            onFocus={this.onFocus}
-            onBlur={this.props.onBlur}
-            handleKeyCommand={this.handleKeyCommand} />
-        </div>
-      </div>
-    )
+    return <div className={styles.editor} {...this.props } />
   }
-}
-
-TextEditor.propTypes = {
-  onChange: PropTypes.func.isRequired
 }
 
 export default TextEditor
