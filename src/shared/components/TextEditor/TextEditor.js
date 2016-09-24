9ba4dd6fd4900ff74f10c5ff3de7f7ba5de8ef41
TaToonz <peerapath.t@gmail.com>
2016-09-24 20:40:54 +0700

    add prop validation for text editor

--- src/shared/components/TextEditor/TextEditor.js
+++ src/shared/components/TextEditor/TextEditor.js
@@ -7,6 +7,11 @@ import 'medium-editor/dist/css/themes/default.css'
 import styles from './TextEditor.scss'
 
 class TextEditor extends Component {
+  static propTypes = {
+    label: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+  }
+
   componentDidMount() {
     if (typeof document !== 'undefined') {
       this.createMediumEditor()
@@ -25,7 +30,7 @@ class TextEditor extends Component {
           'bold', 'italic', 'underline', 'strikethrough',
           'anchor',
           'orderedlist', 'unorderedlist',
-          'h1', 'h2',
+          'h1', 'h2', 'quote',
         ],
       },
       placeholder: {
